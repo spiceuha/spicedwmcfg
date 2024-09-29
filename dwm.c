f2c0887dbbe2ecc72e4e6b26341795b56e99914b
spiceuha <spice@spiceuha.labs>
2024-09-30 00:34:27 +0300

    Changed color scheme, changed default terminal, change some KeyBinds

--- dwm.c
+++ dwm.c
@@ -2030,7 +2030,7 @@ void
 updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
-		strcpy(stext, "dwm-"VERSION);
+		strcpy(stext, "SpiceuhaLabs");
 	drawbar(selmon);
 }
 
